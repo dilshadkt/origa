27e54ca988f7112ecf7fa3b26af2e63b4fe13e52
Dilshad <hmydilshadkt@gmail.com>
2024-06-08 12:10:13 +0530

    feat : meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,29 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "DELTA ORIGA",
+  // metadataBase: new URL("https://www.zigzagdigitalsolutions.com"),
+  twitter: {
+    images: "/logo.svg",
+    description:
+      "Join Origa's community of developers to transform your digital presence. Specializing in web design, mobile app development, SEO, eCommerce, ERP, and digital marketing for business success.",
+    card: "summary_large_image",
+    title:
+      "Origa is a community of developers united to revolutionize the IT industry through collaboration and creativity. We offer web design, mobile app development, SEO, eCommerce, ERP, and digital marketing services.",
+  },
+  title:
+    "Origa - Revolutionizing the IT Industry with Collaborative Innovation",
+  keywords:
+    "Origa, IT industry, web design, mobile app development, SEO, eCommerce development, ERP development, digital marketing, developer community, innovative solutions, business solutions, digital transformation",
+  openGraph: {
+    type: "website",
+    images: "/logo.svg",
+    title:
+      "Origa - Revolutionizing the IT Industry with Collaborative Innovation",
+    description:
+      "Origa is a community of developers united to revolutionize the IT industry through collaboration and creativity. We offer web design, mobile app development, SEO, eCommerce, ERP, and digital marketing services.",
+  },
   description:
-    "Empowering innovation with creative strategies. Committed to excellence, we push boundaries and connect brilliance. Join us in redefining the future of IT.",
+    "Join Origa, a dynamic community of developers, to transform your digital presence. We specialize in web design, mobile app development, SEO, eCommerce, ERP, and digital marketing to drive growth and success.",
 };
 
 export default function RootLayout({
