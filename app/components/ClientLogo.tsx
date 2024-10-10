44a377ee2c65486cee2a5937d05c77ef9b551bfc
origanetworks <origanetworksllp@gmail.com>
2024-10-10 11:55:56 +0530

    message

--- app/components/ClientLogo.tsx
+++ app/components/ClientLogo.tsx
@@ -6,7 +6,7 @@ const ClientLogo = () => {
     <div className="bg-white py-6 sm:py-8 lg:py-12">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
         <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">
-          Trusted by the Origa
+          Partnering for growth
         </h2>
 
         <div className="grid grid-cols-2 gap-6 rounded-lg bg-gray-100 p-6 sm:h-40 sm:content-evenly md:grid-cols-4">
